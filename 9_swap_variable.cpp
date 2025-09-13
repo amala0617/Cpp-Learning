7adf36fd249245c25a6015e72a1e512d89ad7e6a
amala0617 <amalatoms17@gmail.com>
2025-09-13 22:27:37 +0530

    9_swap_variable.cpp

--- 9_swap_variable.cpp
+++ 9_swap_variable.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
-int main(){
-    string var1,var2,temp;
-    cout<<"Enter a variable";
-    cout<<"Enter another variable";
-    cin>>var1>>var2;
-    temp=var1;
-    var1=var2;
-    var2=temp;
-    cout<<"The variables"<<var1<<var2<<"swap to"<<var1<<var2;
+int main() {
+    string var1, var2, temp;
+
+    cout << "Enter first variable: ";
+    cin >> var1;
+    cout << "Enter second variable: ";
+    cin >> var2;
+    temp = var1;
+    var1 = var2;
+    var2 = temp;
+    cout << "After swapping:" << endl;
+    cout << "First variable = " << var1 << endl;
+    cout << "Second variable = " << var2 << endl;
+    return 0;
 }
