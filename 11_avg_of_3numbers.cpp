c349c3f816d999f4e197d8be2a1ee7c4bd815563
amala0617 <amalatoms17@gmail.com>
2025-09-13 22:55:55 +0530

    Add files via upload

---
+++ 11_avg_of_3numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1,num2,num3,avg;
+    cout<<"Enter first number";
+    cin>>num1;
+    cout<<"Enter second number";
+    cin>>num2;
+    cout<<"Enter third number";
+    cin>>num3;
+    avg=(num1+num2+num3)/3;
+    cout<<avg;
+}
